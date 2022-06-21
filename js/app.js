dcf6fbe4fa6f8edb941004c4b636585f8947f52e
HANDABIN <105402278+HANDABIN@users.noreply.github.com>
2022-06-21 13:55:22 +0900

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -5,15 +5,34 @@ const img_url = [
         'images/kiki4.jpg',
         'images/kiki5.jpg',
         'images/kiki6.jpg',
-
     ];
+
+    //작가명
+    const author = [
+        '스티브 J',
+        '요니 P'
+
+    ]
 
     $(function(){
 
+        // 년/월/일
+        let d = new Date();
+        let year = d.getFullYear();
+        let month = d.getMonth() + 1;
+        let date = d.getDate();
+        console.log(year, month, date);
+        $('.year').html(year);
+        $('.month').html(month);
+        $('.date').html(date);
+
+        
+        
         function changeBg() {
           // 0 ~ 2 랜덤값 생성(이미지 번호)
           let imgNum = Math.floor(Math.random() * 6)
           console.log(imgNum);
+
       
           // 이미지 변경
           $('body').css({
